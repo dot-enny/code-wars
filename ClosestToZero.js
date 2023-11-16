cd9a603394f7bb4d0abf0af2e6033cad571ee51d
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2023-11-16 13:00:41 +0100

    kata-solution: closest to zero https://www.codewars.com/kata/59887207635904314100007b

--- ClosestToZero.js
+++ ClosestToZero.js
@@ -31,3 +31,27 @@ console.log(closest(arr));
 arr = [81, 55, 15, -95, -15, 15];
 console.log(closest(arr))
 
+function closest2 (arr) {
+    let set = new Set(arr);
+    let min = Infinity;
+    for (let i of set) {
+        if (Math.abs(i) < Math.abs(min)) min = i; 
+    };
+
+    if (min != 0 && set.has(-min)) return null;
+    return min;
+};
+
+/** TEST CASES */
+arr = [2, 4, -1, -3];
+console.log(closest2(arr));
+arr = [5, 2, -2];
+console.log(closest2(arr));
+arr = [5, 2, 2];
+console.log(closest2(arr));
+arr = [13, 0, -6];
+console.log(closest2(arr));
+arr = [5, 11, 11, 2, -1, 1];
+console.log(closest2(arr));
+arr = [81, 55, 15, -95, -15, 15];
+console.log(closest2(arr))

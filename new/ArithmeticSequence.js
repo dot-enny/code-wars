669c550f35fc1420767e98b8a5874b59b98714e5
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2023-11-23 19:41:46 +0100

    kata-solution: Arithmetic Sequence!

---
+++ new/ArithmeticSequence.js
@@ -0,0 +1,21 @@
+function nthterm (first, n, c) {
+    let i = 0;
+    let term = first;
+    while(i < n) {
+        term+=c;
+        i++;
+    }
+    return term;
+
+    // BigO(n)
+};
+
+let args = [1, 2, 1];
+console.log(nthterm(...args));
+
+function nthterm2 (first, n, c){
+    return first + (n * c);
+    // BigO(1)
+};
+
+console.log(nthterm2(...args));

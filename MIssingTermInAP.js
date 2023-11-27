23b8b4a5ec471d93e3a0baf7baa869b800c58f3f
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2023-11-27 16:44:07 +0100

    kata-solution: Find the missing term in an Arithmetic Progression.

--- MIssingTermInAP.js
+++ MIssingTermInAP.js
@@ -11,3 +11,47 @@ function findMissing (arr) {
 let arr = [1, 3, 5, 9, 11];
 console.log(findMissing(arr))
 
+function findMissing2 (arr) {
+    let commonDiff = (arr[arr.length - 1] - arr[0]) / arr.length;
+    
+    for (let i = 1; i < arr.length; i++) {
+        let expectedValue = arr[i - 1] + commonDiff;
+        if (arr[i] != expectedValue) return expectedValue;
+    };
+};
+
+console.log(findMissing2(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findMissingFormer (arr) {
+//     let diffHash = {};
+//     let maxDiff = 0;
+//     let minDiff = 0;
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         let diff = arr[i + 1] - arr[i];
+//         maxDiff = diff > maxDiff ? maxDiff = diff : maxDiff = maxDiff;
+//         minDiff = diff < minDiff ? minDiff = diff : minDiff = minDiff;
+//         diffHash[diff] ? diffHash[diff] = diffHash[diff] + 1 : diffHash[diff] = 1
+//     }
+
+//     for (let i = 0; i < arr.length - 1; i++) { 
+//         let diff = arr[i + 1] - arr[i] != maxDiff ? arr[i + 1] - arr[i] : arr[i] - arr[i - 1];;
+//         if (arr[i + 1] - arr[i] == maxDiff) return arr[i] + maxDiff - diff; 
+//     }
+
+// }

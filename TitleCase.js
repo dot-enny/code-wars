633fc5872a92cc2458b92b7d4ec56b7459f34157
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2023-12-03 19:32:45 +0100

    kata-solution: Title Case

--- TitleCase.js
+++ TitleCase.js
@@ -1,23 +1,23 @@
 function titleCase(title, minorWords) {
-    if (title === '') return '';
-  
-    // If minorWords is not provided, treat it as an empty string
-    minorWords = minorWords || '';
-  
-    // Convert both title and minorWords to lowercase to ensure case insensitivity
-    title = title.toLowerCase().split(' ');
-    minorWords = minorWords.toLowerCase().split(' ');
-  
-    // Capitalize the first word, and capitalize other words not in the list of minor words
-    const result = title.map((word, index) => {
-      if (index === 0 || !minorWords.includes(word)) {
-        return word.charAt(0).toUpperCase() + word.slice(1);
-      } else {
-        return word;
-      }
-    });
-  
-    return result.join(' ');
+  if (title === '') return '';
+
+  // If minorWords is not provided, treat it as an empty string
+  minorWords = minorWords || '';
+
+  // Convert both title and minorWords to lowercase to ensure case insensitivity
+  title = title.toLowerCase().split(' ');
+  minorWords = minorWords.toLowerCase().split(' ');
+
+  // Capitalize the first word, and capitalize other words not in the list of minor words
+  const result = title.map((word, index) => {
+    if (index === 0 || !minorWords.includes(word)) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    } else {
+      return word;
+    }
+  });
+
+  return result.join(' ');
 };
 
 console.log(titleCase('a clash of KINGS', 'a an the of'))
